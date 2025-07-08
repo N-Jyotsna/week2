fb6760c5f9d204e197194412bdf68fcf23a32b31
Akshitha-2112 <gannojuakshitha8@gmail.com>
2025-07-09 05:02:25 +0530

    my second commit

--- hello.py
+++ hello.py
@@ -2,6 +2,6 @@ from flask import Flask,render_template,request
 app=Flask(__name__)
 @app.route('/')
 def hello():
-    return('Hello world')
+    return('Welcome to github')
 if __name__=="__main__":
     app.run(debug=True) 
