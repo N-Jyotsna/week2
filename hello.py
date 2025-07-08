7eda266172a75ab4b1b54c30c6ef10b5876208cf
N-Jyotsna <njyotsna2024@gmail.com>
2025-07-09 05:13:13 +0530

    my first commit

--- hello.py
+++ hello.py
@@ -2,6 +2,6 @@ from flask import Flask,render_template,request
 app=Flask(__name__)
 @app.route('/')
 def hello():
-    return('Welcome to github')
+    return('Welcome to cc3')
 if __name__=="__main__":
     app.run(debug=True) 
